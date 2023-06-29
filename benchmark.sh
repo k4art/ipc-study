9748fc822869df8eb4df445f6b70a2507ebf44b8
Artem Kan <kan.art.email@gmail.com>
2023-06-29 20:08:20 +0500

    feat(benchmarks): add some more points

--- benchmark.sh
+++ benchmark.sh
@@ -2,7 +2,7 @@
 
 printf "\t%s\t%s\t\t%s\n" "msg size" "mmap" "socket"
 
-for i in {1,16,64,512,2048,4096,16384};
+for i in {1,8,16,64,128,256,512,1024,2048,4096,16384};
 do
   printf "\t%05s\t\t" $i;
   ./build/bench mmap $i | ./walltime.sh;
